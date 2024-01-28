b945f6415c0d84664e3f3f40f8b64df7dcb25088
tocodevictor1995 <132182198+tocodevictor1995@users.noreply.github.com>
2024-01-28 04:03:03 +0000

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-// copyright year
-document.querySelector('#year').textContent = new Date().getFullYear();
-
 // toggle menu in small view
 const toggleMenu = () => {
     document.querySelector('#menu').classList.toggle('open');
